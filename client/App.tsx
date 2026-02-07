4824eeac07bb562c6ed8b071c5475f2530ec4841
smetehanbey-cyber <s.metehanbey@gmail.com>
2026-02-07 16:41:35 +0300

    Update App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,6 @@
 import "./global.css";
 
 import { createRoot } from "react-dom/client";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -13,7 +12,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
